4d70b52d3c5541d2b10d0d1d370f4f0313aed261
k_james <katelin.james@gmail.com>
2017-02-09 20:59:04 -0800

    2/9/2016
    answers

---
+++ script.js
@@ -0,0 +1,204 @@
+//1
+/*
+var someMonth;
+function (theMonth);
+var currentMonth;
+function (myLibrary);
+*/
+
+//2
+/*
+var a, b, c, d;
+a = 4;
+b = "katelin";
+c = null;
+d = false;
+*/
+
+//3
+/*
+var x = new Date();
+alert(x.toString());
+
+var userNumber = prompt("choose a number with a decimal");
+alert(Math.round(userNumber));
+*/
+
+//4
+/*
+var firstName;
+var lastName;
+var address;
+var city;
+var state;
+var zipCode;
+var yourAge;
+var refferalSource;
+var mayWeContactYou;
+*/
+
+//5
+/*
+var firstName, lastName, address, city, state, zipCode, yourAge, refferalSource, mayWeContactYou;
+firstName = "whats your name";
+lastName = "whats your last name";
+address = "address";
+city = "city";
+state = "state";
+zipCode = "zipCode";
+yourAge = "age";
+refferalSource = "source";
+mayWeContactYou = true;
+
+var firstName = "whats your name";
+var lastName = "last name";
+var address = "address";
+var city = "city";
+var state = "state";
+var zipCode = "zip code";
+var yourAge = "age";
+var refferalSource = "source";
+var mayWeContactYou = true;
+
+var firstName = "what's your name", lastName = "lastname", address = "address", city = "city", state = "state", zipCode = "zip code", yourAge = "age", refferalSource = "source", mayWeContactYou = true;
+*/
+
+//6
+/*
+var intAge = 21;
+window.console.log("katelin is " + intAge);
+
+var bln = true;
+window.console.log("the answer is " + bln);
+
+var bln = true;
+window.console.log(bln + 10);
+*/
+
+//7
+/*
+var strFirstName = "katelin";
+window.console.log(strFirstName + strLastName);
+var strLastName = "james";
+*/
+
+//8
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+window.console.log(someString);
+*/
+
+//9
+/*
+var x = null;
+var y;
+window.console.log(x, y);
+*/
+
+//10
+/*
+window.console.log(
+typeof "katelin james",
+typeof 21,
+typeof true,
+typeof null,
+typeof cats);
+*/
+
+//11
+/*
+window.alert("hello zac ruvalcaba" + "," + " welcome to the javascript class");
+*/
+
+//12
+/*
+var name = "Katelin";
+window.alert("hello " + name + "," + " welcome to the javascript class");
+*/
+
+//13
+/*
+var course = "javaScript";
+var name = "Katelin";
+window.alert("hello " + name + "," + " welcome to the " + course + " class");
+*/
+
+//14
+/*
+var course = "javaScript";
+var name = "Katelin";
+window.alert("Hello " + name + ", \n welcome to the " + course + " class");
+*/
+
+//15
+/*
+var userName = window.prompt("What's your name?");
+var course = "javaScript";
+window.alert("Hello " + userName+ ", \n welcome to the " + course + " class");
+*/
+
+//16
+/*
+var userName = window.prompt("What's your name?");
+var course = window.prompt("What class are you taking?");
+window.alert("Hello " + userName+ ", \n welcome to the " + course + " class");
+*/
+
+//17
+/*
+var x = 10;
+var y = 20;
+window.console.log(x + y);
+*/
+
+//18
+/*
+var x = 20;
+window.console.log(x += x);
+*/
+
+//19
+/*
+var x = 20;
+window.console.log(x *= 5);
+*/
+
+//20
+/*
+var x = 20;
+x /= 3;
+window.console.log(x);
+*/
+
+//21
+/*
+var x = 10;
+var y = 10;
+window.console.log(x == y);
+*/
+
+//22
+/*
+var x = 10;
+var y = 20;
+window.console.log(x > y);
+*/
+
+//23
+/*
+var widget = new Object();
+window.console.log(typeof widget);
+*/
+
+//24
+/*
+var widget = new Object();
+window.console.log("PI" in Math);
+*/
+//25
+/*
+var widget = new Object();
+window.console.log("widget" in Object);
+*/
+
+
